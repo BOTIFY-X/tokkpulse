dfdad4e795d08264e9e8c7fc7d695106cad67927
BOTIFY-X <bendelastdon@gmail.com>
2026-06-20 17:16:56 +0100

    Delete tokpulse-railway/tokpulse-railway directory

--- tokpulse-railway/tokpulse-railway/lib/db/src/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./users";
-export * from "./tiktok_accounts";
-export * from "./video_analytics";
-export * from "./growth_snapshots";
-export * from "./payments";
-export * from "./push_subscriptions";
